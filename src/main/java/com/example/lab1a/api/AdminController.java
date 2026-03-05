e4e11d2d3f5eb5282c86552ffc600aef8a36c545
K00326801 <k00326801@student.tus.ie>
2026-03-05 14:01:16 +0000

    commiting AdminController.java

--- src/main/java/com/example/lab1a/api/AdminController.java
+++ src/main/java/com/example/lab1a/api/AdminController.java
@@ -1,7 +1,9 @@
 package com.example.lab1a.api;
 
 import com.example.lab1a.domain.Location;
+import com.example.lab1a.domain.Restaurant;
 import com.example.lab1a.service.LocationService;
+import com.example.lab1a.service.RestaurantService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,9 +14,11 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:5173")
 public class AdminController {
     private final LocationService locationService;
+    private final RestaurantService restaurantService;
 
-    public AdminController(LocationService locationService) {
+    public AdminController(LocationService locationService, RestaurantService restaurantService) {
         this.locationService = locationService;
+        this.restaurantService = restaurantService;
     }
 
     @GetMapping
@@ -31,5 +35,16 @@ public class AdminController {
     @ResponseStatus(HttpStatus.CREATED)
     public Location createLocation(@RequestBody Location location) {
         return locationService.createLocation(location);
+    }
+
+    @GetMapping("restaurants/{locationId}")
+    public List<Restaurant> findRestaurantByLocationId(@PathVariable Long locationId) {
+        return restaurantService.getRestaurantsByLocation(locationId);
+    }
+
+    @PostMapping("/restaurant")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Restaurant createRestaurant(@RequestBody Restaurant restaurant, Long locationId) {
+        return restaurantService.createRestaurant(locationId, restaurant);
     }
 }
